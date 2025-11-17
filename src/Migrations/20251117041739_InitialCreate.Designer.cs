5ce1bd3735dd3470233d9c42137444a1e1a89647
leomotalima <leomota436@gmail.com>
2025-11-17 01:20:17 -0300

    Add InitialCreate migration

--- src/Migrations/20251117041739_InitialCreate.Designer.cs
+++ src/Migrations/20251117041739_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using JobFitScoreAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oracle.EntityFrameworkCore.Metadata;
 
@@ -11,9 +12,11 @@ using Oracle.EntityFrameworkCore.Metadata;
 namespace JobFitScoreAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251117041739_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
